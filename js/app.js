ac9c6ab7e91d9c595496fb33e9afe7b0dfafc34a
DrazhinUstin <zmagar91@gmail.com>
2021-10-14 21:50:14 +0300

    Added preloader. Update of js structure.

--- js/app.js
+++ js/app.js
@@ -1,66 +1,7 @@
-import fetchData from './modules/fetchData.js';
-import displayData from './modules/displayData.js';
+import setupSearch from "./modules/setupSearch.js";
 
-const startAPP = () => {
+document.addEventListener('DOMContentLoaded', setupSearch);
 
-    // DOM elements
-    const form = document.querySelector('.search-form');
-    const input = form.querySelector('input');
-    const messageDOM = document.querySelector('.message');
-    const buttonsDOM = [...document.querySelectorAll('.btn-container')];
-    const clearBtn = document.querySelector('.clear-btn');
-    const audioBtn = document.querySelector('.audio-btn');
-    // This we will get dynamically
-    let audioTrack;
-
-    // Arrow functions
-    const startSearch = async (value) => {
-        messageDOM.classList.remove('hide');
-        messageDOM.innerHTML = `Searching for a word <span>${value}</span>...`;
-        const data = await fetchData(value);
-        if (data.title) {
-            messageDOM.innerHTML = `Sorry, no results for word <span>${value}</span>... You can try to enter another word.`;
-        } else {
-            displayData(data);
-            setupAudio(data);
-            messageDOM.classList.add('hide');
-        }
-    };
-
-    const setupAudio = (data) => {
-        audioTrack = null;
-        const audio = data[0].phonetics[0] ? data[0].phonetics[0]['audio'] : null;
-        if (!audio) return;
-        const audioURL = `https:${audio}`;
-        audioTrack = new Audio(audioURL);
-    };
-
-    // Event Listeners
-    form.addEventListener('submit', event => {
-        event.preventDefault();
-        const value = input.value;
-        input.value = '';
-        input.blur();
-        startSearch(value);
-    });
-
-    clearBtn.addEventListener('click', () => {
-        input.value = '';
-    });
-
-    audioBtn.addEventListener('click', () => {
-        if (!audioTrack) return;
-        audioTrack.play();
-    });
-
-    buttonsDOM.forEach(elem => {
-        elem.addEventListener('click', event => {
-            if (event.target.tagName !== 'BUTTON') return;
-            const value = event.target.textContent;
-            startSearch(value);
-        });
-    });
-
-};
-
-startAPP();
+window.addEventListener('load', () => {
+    setTimeout(() => document.querySelector('.preloader').classList.add('hide'), 200);
+});
